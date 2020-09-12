d9e10ee58fd0ad91ec4959e1732b983dcddf9981
P24980 <P24980@UN990155>
2020-09-12 15:54:05 +0100

    remove environs file

--- cmd/daemon/set_environs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export GO_GET_GIT_RABBIT_QUEUE_URL="amqp://guest:guest@localhost:5672/"
-export GO_GET_GIT_QUEUE_NAME="go-get-git-daemon-events"
-export GO_GET_GIT_EVENT_EXCHANGE_NAME="events"
-export GO_GET_GIT_EXCHANGE_TYPE="fanout"
